16d07092a4b91031441fe652486e570847e69b8d
Martin Chlebovec <martinius96@gmail.com>
2021-01-31 14:25:04 +0100

    Create ESP32_MQTTS_client.ino

---
+++ examples/ESP32_MQTTS_client/ESP32_MQTTS_client.ino
@@ -0,0 +1,164 @@
+/*|-----------------------------------------------------|*/
+/*|Projekt: JSON client - ESP32 - MQTTS                 |*/
+/*|Popis: Pripojenie k Ethernet termostatu v LAN sieti  |*/
+/*|stiahnutie JSON dat, vyparsovanie hodnot, vypis      |*/
+/*|Odoslanie dat do troch topicov na free MQTT          |*/
+/*|Broker pre vývojárov - slovenský IoT Industries      |*/
+/*|Autor: Martin Chlebovec                              |*/
+/*|E-mail: martinius96@gmail.com                        |*/
+/*|Revízia: 29. Januar 2021                             |*/
+/*|-----------------------------------------------------|*/
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+const char* ssid = "WIFI_NAME"; //SSID vasej WiFi siete
+const char* password = "WIFI_PASSWORD"; //HESLO vasej WiFi siete
+const char* host = "192.168.1.XX"; //IP adresa Ethernet termostatu
+const char* mqtt_server = "mqtt.iotindustries.sk";
+const int httpPort = 80;
+WiFiClient klient; //pre HTTP
+WiFiClientSecure   espClient; //pre MQTTS
+PubSubClient client(espClient);
+unsigned long timer = 0;
+int interval = 15000;
+// Root CA
+const static char* test_root_ca PROGMEM = \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIFdzCCBF+gAwIBAgIQE+oocFv07O0MNmMJgGFDNjANBgkqhkiG9w0BAQwFADBv\n" \
+    "MQswCQYDVQQGEwJTRTEUMBIGA1UEChMLQWRkVHJ1c3QgQUIxJjAkBgNVBAsTHUFk\n" \
+    "ZFRydXN0IEV4dGVybmFsIFRUUCBOZXR3b3JrMSIwIAYDVQQDExlBZGRUcnVzdCBF\n" \
+    "eHRlcm5hbCBDQSBSb290MB4XDTAwMDUzMDEwNDgzOFoXDTIwMDUzMDEwNDgzOFow\n" \
+    "gYgxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpOZXcgSmVyc2V5MRQwEgYDVQQHEwtK\n" \
+    "ZXJzZXkgQ2l0eTEeMBwGA1UEChMVVGhlIFVTRVJUUlVTVCBOZXR3b3JrMS4wLAYD\n" \
+    "VQQDEyVVU0VSVHJ1c3QgUlNBIENlcnRpZmljYXRpb24gQXV0aG9yaXR5MIICIjAN\n" \
+    "BgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAgBJlFzYOw9sIs9CsVw127c0n00yt\n" \
+    "UINh4qogTQktZAnczomfzD2p7PbPwdzx07HWezcoEStH2jnGvDoZtF+mvX2do2NC\n" \
+    "tnbyqTsrkfjib9DsFiCQCT7i6HTJGLSR1GJk23+jBvGIGGqQIjy8/hPwhxR79uQf\n" \
+    "jtTkUcYRZ0YIUcuGFFQ/vDP+fmyc/xadGL1RjjWmp2bIcmfbIWax1Jt4A8BQOujM\n" \
+    "8Ny8nkz+rwWWNR9XWrf/zvk9tyy29lTdyOcSOk2uTIq3XJq0tyA9yn8iNK5+O2hm\n" \
+    "AUTnAU5GU5szYPeUvlM3kHND8zLDU+/bqv50TmnHa4xgk97Exwzf4TKuzJM7UXiV\n" \
+    "Z4vuPVb+DNBpDxsP8yUmazNt925H+nND5X4OpWaxKXwyhGNVicQNwZNUMBkTrNN9\n" \
+    "N6frXTpsNVzbQdcS2qlJC9/YgIoJk2KOtWbPJYjNhLixP6Q5D9kCnusSTJV882sF\n" \
+    "qV4Wg8y4Z+LoE53MW4LTTLPtW//e5XOsIzstAL81VXQJSdhJWBp/kjbmUZIO8yZ9\n" \
+    "HE0XvMnsQybQv0FfQKlERPSZ51eHnlAfV1SoPv10Yy+xUGUJ5lhCLkMaTLTwJUdZ\n" \
+    "+gQek9QmRkpQgbLevni3/GcV4clXhB4PY9bpYrrWX1Uu6lzGKAgEJTm4Diup8kyX\n" \
+    "HAc/DVL17e8vgg8CAwEAAaOB9DCB8TAfBgNVHSMEGDAWgBStvZh6NLQm9/rEJlTv\n" \
+    "A73gJMtUGjAdBgNVHQ4EFgQUU3m/WqorSs9UgOHYm8Cd8rIDZsswDgYDVR0PAQH/\n" \
+    "BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wEQYDVR0gBAowCDAGBgRVHSAAMEQGA1Ud\n" \
+    "HwQ9MDswOaA3oDWGM2h0dHA6Ly9jcmwudXNlcnRydXN0LmNvbS9BZGRUcnVzdEV4\n" \
+    "dGVybmFsQ0FSb290LmNybDA1BggrBgEFBQcBAQQpMCcwJQYIKwYBBQUHMAGGGWh0\n" \
+    "dHA6Ly9vY3NwLnVzZXJ0cnVzdC5jb20wDQYJKoZIhvcNAQEMBQADggEBAJNl9jeD\n" \
+    "lQ9ew4IcH9Z35zyKwKoJ8OkLJvHgwmp1ocd5yblSYMgpEg7wrQPWCcR23+WmgZWn\n" \
+    "RtqCV6mVksW2jwMibDN3wXsyF24HzloUQToFJBv2FAY7qCUkDrvMKnXduXBBP3zQ\n" \
+    "YzYhBx9G/2CkkeFnvN4ffhkUyWNnkepnB2u0j4vAbkN9w6GAbLIevFOFfdyQoaS8\n" \
+    "Le9Gclc1Bb+7RrtubTeZtv8jkpHGbkD4jylW6l/VXxRTrPBPYer3IsynVgviuDQf\n" \
+    "Jtl7GQVoP7o81DgGotPmjw7jtHFtQELFhLRAlSv0ZaBIefYdgWOWnU914Ph85I6p\n" \
+    "0fKtirOMxyHNwu8=\n" \
+    "-----END CERTIFICATE-----\n";
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32_Client_Termostat")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("termostat/#");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200); //rychlost seriovej linky
+  Serial.println();
+  espClient.setCACert(test_root_ca);
+  Serial.print("Pripajanie na WiFi siet: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password); //pripoj sa na wifi siet s heslom
+  while (WiFi.status() != WL_CONNECTED) { //pokial sa nepripojime na wifi opakuj pripajanie a spustaj funkcie pre ovladanie v offline rezime
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Wifi pripojene s IP:");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, 8883);
+  client.setCallback(callback);
+  if (!client.connected()) {
+    reconnect();
+  }
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+  }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  client.loop();
+  if ((millis() - timer) >= interval || timer == 0) {
+    timer = millis();
+    klient.stop();
+    if (klient.connect(host, httpPort)) {
+      String url = "/get_data.json";
+      Serial.println("Pripojenie uspesne, nacitavam JSON data");
+      klient.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "User-Agent: ESP32\r\n" + "Connection: close\r\n\r\n");
+      while (klient.connected()) {
+        String line = klient.readStringUntil('\n'); //HTTP HEADER
+        //Serial.println(line);
+        if (line == "\r") {
+          break;
+        }
+      }
+      DynamicJsonDocument doc(512);
+      String line = klient.readString(); //PAYLOAD
+      Serial.println(line);
+      deserializeJson(doc, line);
+      JsonObject obj = doc.as<JsonObject>();
+      float hystereza = obj[String("Hysteresis")];
+      float cielova_teplota = obj[String("Target_Temperature")];
+      float actual_temperature = obj[String("Actual_Temperature")];
+      Serial.print("Hystereza: ");
+      Serial.println(hystereza);
+      char pole_hysteresis[32];
+      dtostrf(hystereza, 1, 2, pole_hysteresis);
+      client.publish("termostat/hysteresis", pole_hysteresis);
+      Serial.print("Cielova teplota: ");
+      Serial.println(cielova_teplota);
+      char pole_cielova_teplota[32];
+      dtostrf(cielova_teplota, 1, 2, pole_cielova_teplota);
+      client.publish("termostat/target_temp", pole_cielova_teplota);
+      Serial.print("Namerana (aktualna) teplota: ");
+      Serial.println(actual_temperature);
+      char pole_actual_teplota[32];
+      dtostrf(actual_temperature, 1, 2, pole_actual_teplota);
+      client.publish("termostat/actual_temp", pole_actual_teplota);
+    } else if (!klient.connect(host, httpPort)) {
+      Serial.println("Nepodarilo sa pripojenie k termostatu, ani nacitanie JSON data");
+    }
+  }
+}
